f13c5d44beaefff9e09e50267f81ca401c8049f6
Javier Arques Tortosa <arquesjavier@gmail.com>
2024-06-12 12:27:39 +0200

    actualizada tabla User

--- src/api/app.py
+++ src/api/app.py
@@ -12,7 +12,7 @@ from routes.shared_data import shared_data_api
 from routes.favorite_data import favorites_api
 from routes.address_data import address_api
 from routes.bank_data import bank_data_api
-from routes.content import content_api
+# from routes.content import content_api
 
 from config import Config
 from cors import init_cors
@@ -51,7 +51,7 @@ def create_app():
     app.register_blueprint(favorites_api)
     app.register_blueprint(address_api)
     app.register_blueprint(bank_data_api)
-    app.register_blueprint(content_api)
+    # app.register_blueprint(content_api)
     # Inicializar la interfaz de administración
     init_admin(app)
 
